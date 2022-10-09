796dc439005f2d8fc4752c29f5c97532d8a2903b
Min Htet Ko <penguincrab99@gmail.com>
2022-10-09 13:18:28 +0630

    First Commit (EAS Update)

---
+++ app.config.js
@@ -0,0 +1,23 @@
+export default function (config) {
+    return {
+        ...config,
+        name: process.env === "production" ? "Simple Chat" : "Simple Chat Dev",
+        android: {
+            package:
+                process.env === "production"
+                    ? "com.penguincrab.simple_chat"
+                    : "com.penguincrab.simple_chat_dev",
+        },
+        runtimeVersion: {
+            policy: "sdkVersion",
+        },
+        updates: {
+            url: "https://u.expo.dev/63cb4453-e4ce-46d2-881f-28f333896063",
+        },
+        extra: {
+            eas: {
+                projectId: "63cb4453-e4ce-46d2-881f-28f333896063",
+            },
+        },
+    };
+}
